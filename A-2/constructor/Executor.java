bb89cf6e1328c8940100b428170e0d3c3ac05dc7
Nandhakumaran <148994365+Dev-IamNk@users.noreply.github.com>
2025-09-09 13:51:28 +0530

    Update Executor.java

--- A-2/constructor/Executor.java
+++ A-2/constructor/Executor.java
@@ -4,9 +4,9 @@ public class Executor {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s1=new Student(1,"Pooja","JFS");
-		Student s2=new Student(2,"Navvin","JFS");
-		Student s3=new Student(3,"Fowzz","DESIGNER");
+		Student s1=new Student(1,"NANDHA","JFS");
+		Student s2=new Student(2,"KUMARAN","JFS");
+		Student s3=new Student(3,"RAJESH","DESIGNER");
 		
 		
 		s1.displayInfo();
